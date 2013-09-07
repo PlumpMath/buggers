d732d534415d1faf7364fa3316993d419791ed45
Stephen Olsen <steve.olsen@gmail.com>
2013-09-07 18:41:30 -0400

    whoops, forgot to save...

--- buggers.rkt
+++ buggers.rkt
@@ -2,48 +2,86 @@
 #| 
    Working my way through realm of racket for a bit then making a game called
    lil buggers which is going to be real fun.
-
-   Might write a little 2d physics engine just to practice.
 |#
-
 (require 2htdp/universe 2htdp/image)
 
 (define SCREEN-WIDTH 1024)
 (define SCREEN-HEIGHT 600)
 (define PLAYER-SPEED 10)
 
-;; Buggers!
-(struct gamestate (buggers keysdown) #:transparent)
-(struct bugger (velocity position icon) #:transparent)
+;; GameState
+(struct gamestate (entities keysdown) #:transparent)
 
-;; Draw
+;; Entities
+(struct entity (id components) #:transparent)
+
+;; Components
+(struct position (val) #:mutable #:transparent)
+(struct velocity (val) #:mutable #:transparent)
+(struct icon (val) #:mutable #:transparent)
+(struct player () #:transparent)
+
+(define (is-component-type t c)
+    (let ([checker
+           (cond
+             [(equal? t position) position?]
+             [(equal? t velocity) velocity?]
+             [(equal? t icon) icon?]
+             [(equal? t player) player?])])
+      (checker c)))
+
+(define (get-with-components entities comp-types)
+  (define (has-component? entity comp-type)
+    (ormap
+     (λ (component) (is-component-type comp-type component))
+     (entity-components entity)))
+  (filter
+   (λ (entity)
+     (andmap (curry has-component? entity) comp-types))
+   entities))
+
+;; Get a component from an entity
+(define (get-component ent comp-type)
+  (let ([comp-list (filter
+                    (curry is-component-type comp-type)
+                    (entity-components ent))])
+    (if (empty? comp-list)
+        #f
+        (first comp-list))))
+
+;; Returns the player entity
+(define (get-player ents)
+  (first (get-with-components ents (list player))))
+
+;; System: Draw
+;; Comps: icon, position
 (define (compute-screen-position center pos)
   (let ([x-offset (- (first center) (/ SCREEN-WIDTH 2))]
         [y-offset (- (second center) (/ SCREEN-HEIGHT 2))])
     (list (- (first pos) x-offset)
           (- (second pos) y-offset))))
 
-(define (draw-buggers buggers)
-  (let* ([player (first buggers)]
-         [player-position (bugger-position player)])
-    (foldl (lambda (b screen)
-             (let* ([img (bugger-icon b)]
-                    [position (bugger-position b)]
+(define (draw-icons ents)
+  (let* ([player (get-player ents)]
+         [player-position (position-val (get-component player position))]
+         [ents-with-icons (get-with-components ents (list icon))])
+    (foldl (λ (e screen)
+             (let* ([img (icon-val (get-component e icon))]
+                    [position (position-val (get-component e position))]
                     [draw-at (compute-screen-position player-position position)]
                     [x (first draw-at)]
                     [y (second draw-at)])
                (place-image img x y screen)))
            (empty-scene SCREEN-WIDTH SCREEN-HEIGHT)
-           buggers)))
+           ents-with-icons)))
 
 (define (render-game state)
-  (draw-buggers (gamestate-buggers state)))
+  (draw-icons (gamestate-entities state)))
 
 ;; Update
-;; TODO
 (define (get-player-velocity keys-down)
   (let ([apply-directional-velociy
-         (lambda (k v)
+         (λ (k v)
            (cond
              [(key=? k "w") (map + v '(0 -1))]
              [(key=? k "a") (map + v '(-1 0))]
@@ -53,29 +91,49 @@
     (map (curry * PLAYER-SPEED)
          (foldl apply-directional-velociy '(0 0) keys-down))))
 
-(define (apply-player-velocity w)
+;; Takes the list of entities, replaces the component of comp-type 
+;; on the entity with id id with new-component.
+;; Returns a new list of entities.
+;; This can be used to swap non mutable components but with mutable ones
+;; it's pretty overkill.
+(define (swap-component ents ent-id comp-type new-comp)
+  (let* ([ent (first (filter (λ (e) (equal? (entity-id e) ent-id)) ents))]
+         [others (filter (λ (e) (not (equal? (entity-id e) ent-id))) ents)]
+         [comps (filter 
+                 (λ (c) (not (is-component-type comp-type c)))
+                 (entity-components ent))]
+         [new-ent (struct-copy entity ent
+                               [components (cons new-comp comps)])])
+    (cons new-ent others)))
+
+;; System
+;; Comps: velocity player
+(define (set-player-velocity w)
   (let* ([ks (gamestate-keysdown w)]
          [v (get-player-velocity ks)]
-         [bs (gamestate-buggers w)]
-         [player (first bs)])
-    (struct-copy gamestate w
-                 [buggers
-                  (cons
-                   (struct-copy bugger player [velocity v])
-                   (rest (gamestate-buggers w)))])))
+         [ents (gamestate-entities w)]
+         [player (get-player ents)]
+         [players-current-velocity (get-component player velocity)])
+    (set-velocity-val! players-current-velocity v)
+    w))
 
-(define (update-bugger-position b)
-  (let ([v (bugger-velocity b)]
-        [p (bugger-position b)])
-    (struct-copy bugger b [position (map + v p)])))
+(define (update-position! e)
+  (let ([v (get-component e velocity)]
+        [p (get-component e position)])
+    (and v p (set-position-val! p (map +
+                                       (velocity-val v)
+                                       (position-val p))))))
 
-(define (update-buggers-positions w)
-  (let* ([buggers (gamestate-buggers w)]
-         [updated-buggers (map update-bugger-position buggers)])
-    (struct-copy gamestate w [buggers updated-buggers])))
+;; System
+;; Comps: velocity position
+(define (apply-velocity-to-position w)
+  (let* ([ents (gamestate-entities w)])
+    (for ([e ents])
+      (update-position! e))
+    w))
 
 (define (update-game w)
-  ((compose update-buggers-positions apply-player-velocity) w))
+  ((compose apply-velocity-to-position set-player-velocity) w))
 
 ;; KeyPresses
 (define (keydown w a-key)
@@ -87,14 +145,20 @@
 (define (keyup w a-key)
   (let ([keys (gamestate-keysdown w)])
     (struct-copy gamestate w
-                 [keysdown (filter (lambda (k) (not (key=? a-key k))) keys)])))
+                 [keysdown (filter (λ (k) (not (key=? a-key k))) keys)])))
 
 (define (start-scene)
-  (big-bang (gamestate (list (bugger '(0 0) '(0 0) (circle 20 100 "blue"))
-                             (bugger '(0 0) '(900 502) (circle 14 100 "red"))
-                             (bugger '(0 0) '(807 40) (circle 6 100 "green"))
-                             (bugger '(0 0) '(225 120) (circle 88 100 "black")))
-                       '()) ;;Keys Down
+  (big-bang (gamestate (list (entity 1 (list (player)
+                                             (position '(0 0))
+                                             (velocity '(0 0))
+                                             (icon (circle 20 100 "blue"))))
+                             (entity 2 (list (position '(900 502))
+                                             (icon (circle 14 100 "red"))))
+                             (entity 3 (list (position '(807 40))
+                                             (icon (circle 6 100 "green"))))
+                             (entity 4 (list (position '(225 120))
+                                             (icon (circle 88 100 "black")))))
+                       '())
             (on-tick update-game)
             (on-key keydown)
             (on-release keyup)
