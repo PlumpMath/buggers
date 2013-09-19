756ccbec55604bf4aaaa3b0aedc9a895705f29fc
Stephen Olsen <steve.olsen@gmail.com>
2013-09-19 14:19:31 -0400

    broke out rendering to own file

--- clojure/src/buggers/core.clj
+++ clojure/src/buggers/core.clj
@@ -2,17 +2,12 @@
   (:require
    [buggers.systems :as sys]
    [buggers.systems.terrain :refer [terrain-system]]
-   [clojure.math.numeric-tower :as math]
-   [clojure.data.json :as json])
+   [buggers.render :as r])
   (:gen-class)
-  (:import (SimplexNoise)
-           (com.badlogic.gdx ApplicationListener Gdx)
-           (com.badlogic.gdx.files FileHandle)
-           (com.badlogic.gdx Input$Keys)
-           (com.badlogic.gdx.graphics GL10 Mesh VertexAttribute
-                                      Texture)
-           (com.badlogic.gdx.graphics.g2d SpriteBatch TextureRegion)
-           (com.badlogic.gdx.backends.lwjgl LwjglApplication)))
+  (:import
+   (com.badlogic.gdx ApplicationListener Gdx)
+   (com.badlogic.gdx Input$Keys)
+   (com.badlogic.gdx.backends.lwjgl LwjglApplication)))
 
 ;; Not sure where this goes yet.
 
@@ -31,46 +26,6 @@
 (def test-systems
   [(terrain-system)])
 
-;; Drawing
-;; =======
-
-
-;; Textures
-;; ========
-(defn get-texture-locations []
-  (let [get-frames #(get % "frames")]
-    (->> (clojure.java.io/resource "planetcute/planetcute.json")
-         slurp
-         json/read-str
-         get-frames
-         (map #(vector (get % "filename") (get % "frame")))
-         (map (fn [[k v]]
-                (vector (-> k
-                            clojure.string/lower-case
-                            (clojure.string/replace ".png" "")
-                            (clojure.string/replace " " "-")
-                            keyword)
-                        [(get v "x")
-                         (get v "y")
-                         (get v "w")
-                         (get v "h")])))
-         (into {})
-         )))
-
-(defn texture-region [texture x y w h]
-  (TextureRegion. texture x y w h))
-
-(defn planetcute-textures
-  "Loads the planetcute texture and computes the texture regions."
-  []
-  (let [planetcute-file (FileHandle.
-                         (clojure.java.io/file
-                          (clojure.java.io/resource "planetcute/planetcute.png")))
-        texture (Texture. planetcute-file)]
-    (into {}
-          (map
-           (fn [[k v]] (vector k (apply texture-region texture v)))
-           (get-texture-locations)))))
 
 ;; SCENE
 ;; =====
@@ -82,42 +37,16 @@
   (render [this] "Draw current scene")
   (dispose [this] "not sure yet..."))
 
-(defn to-screen-space
-  "Scales a position in game-space to screen-space"
-  [pos]
-  (let [scale-x 100
-        scale-y 80
-        scale-z -40
-        [x y z] pos]
-    [(* scale-x x)
-     (+ (* scale-y y)
-        (* scale-z z))]))
-
-(defn draw-position
-  "Calculates where a tile is drawn based on world scale, center screen
-   and the width and height of the texture."
-  [screen-width screen-height center pos w h]
-  (let [[cx cy] (to-screen-space center)
-        [px py] (to-screen-space pos)
-        x-offset (- cx (/ screen-width 2))
-        y-offset (- cy (/ screen-height 2))]
-    [(- (- px x-offset) (/ w 2))
-     (- (- py y-offset) (/ h 2))]))
-
-
 (deftype BuggersMainScene [gamestate
                            systems
-                           draw-mesh
-                           textures
-                           sprite-batch]
+                           renderer]
   LibGDXScene
   (initialize [_]
     ;; Test Scene
     (reset! gamestate test-scene)
     (reset! systems test-systems)
     ;; Set up graphics.
-    (dosync (ref-set sprite-batch (SpriteBatch.)))
-    (dosync (ref-set textures (planetcute-textures)))
+    (reset! renderer (r/create-renderer))
     )
   (pause [_] nil)
   (resize [_ w h] nil)
@@ -147,54 +76,17 @@
     (doseq [s @systems]
       (swap! gamestate (partial sys/run s)))
 
-    ;; Drawing
-    (let [screen-width (.getWidth Gdx/graphics)
-          screen-height (.getHeight Gdx/graphics)
-          world @gamestate
-          center (get-in world [:entities :player :position])
-          [cx cy _] center
-          range-x (map int (range
-                            (math/floor (- cx (/ (/ screen-width 100) 1.5)))
-                            (math/ceil (+ cx (/ (/ screen-width 100) 1.5)))))
-          range-y (map int (range
-                            (math/floor (- cy (/ (/ screen-height 80) 1.5)))
-                            (math/ceil (+ cy (/ (/ screen-height 80) 1.5)))))]
-
-      ;; Clear Screen
-      (doto (Gdx/gl)
-        (.glClear GL10/GL_COLOR_BUFFER_BIT))
-
-      ;; Draw
-      (.begin @sprite-batch)
-      ;; Draw Visable Terrain
-      
-      (doseq [y (reverse range-y)
-              x range-x]
-        (let [terrain-type (get-in world [:terrain [x y]])
-              [x y] (draw-position screen-width screen-height center [x y 0] 100 120)]
-          (when terrain-type
-            (.draw @sprite-batch (terrain-type @textures) (float x) (float y)))))
-
-      ;; Draw Player
-      (let [[x y] (draw-position screen-width screen-height center [cx cy 0] 77 91)]
-        (.draw @sprite-batch
-               (:character-horn-girl @textures) (float x) (float y)))
-      (.end @sprite-batch)
-      ))
-   
+    ;; Draw!
+    (r/render @renderer @gamestate)
+   )
   (dispose [_] nil))
-
-;; MAIN (or core?)
-;; ===============
 
 (defn app-listener
   "Creates an ApplicationListner instance that represents the game."
   [scene]
   (let [scene (BuggersMainScene. (atom {})
                                  (atom [])
-                                 (ref nil)
-                                 (ref nil)
-                                 (ref nil))]
+                                 (atom nil))]
     {:listener (proxy [ApplicationListener] []
                  (resize [w h] (resize scene w h))
                  (create [] (initialize scene))
---
+++ clojure/src/buggers/render.clj
@@ -0,0 +1,112 @@
+(ns buggers.render
+  (:require
+   [clojure.math.numeric-tower :as math]
+   [clojure.data.json :as json])
+  (:import
+   (com.badlogic.gdx Gdx)
+   (com.badlogic.gdx.files FileHandle)
+   (com.badlogic.gdx.graphics GL10 Mesh VertexAttribute
+                              Texture)
+   (com.badlogic.gdx.graphics.g2d SpriteBatch TextureRegion)
+   )
+  )
+
+;; Load textures.
+(defn get-texture-locations []
+  (let [get-frames #(get % "frames")]
+    (->> (clojure.java.io/resource "planetcute/planetcute.json")
+         slurp
+         json/read-str
+         get-frames
+         (map #(vector (get % "filename") (get % "frame")))
+         (map (fn [[k v]]
+                (vector (-> k
+                            clojure.string/lower-case
+                            (clojure.string/replace ".png" "")
+                            (clojure.string/replace " " "-")
+                            keyword)
+                        [(get v "x")
+                         (get v "y")
+                         (get v "w")
+                         (get v "h")])))
+         (into {})
+         )))
+
+(defn texture-region [texture x y w h]
+  (TextureRegion. texture x y w h))
+
+(defn planetcute-textures
+  "Loads the planetcute texture and computes the texture regions."
+  []
+  (let [planetcute-file (FileHandle.
+                         (clojure.java.io/file
+                          (clojure.java.io/resource "planetcute/planetcute.png")))
+        texture (Texture. planetcute-file)]
+    (into {}
+          (map
+           (fn [[k v]] (vector k (apply texture-region texture v)))
+           (get-texture-locations)))))
+
+;; Computing Draw Position.
+(defn to-screen-space
+  "Scales a position in game-space to screen-space"
+  [pos]
+  (let [scale-x 100
+        scale-y 80
+        scale-z -40
+        [x y z] pos]
+    [(* scale-x x)
+     (+ (* scale-y y)
+        (* scale-z z))]))
+
+(defn draw-position
+  "Calculates where a tile is drawn based on world scale, center screen
+   and the width and height of the texture."
+  [screen-width screen-height center pos w h]
+  (let [[cx cy] (to-screen-space center)
+        [px py] (to-screen-space pos)
+        x-offset (- cx (/ screen-width 2))
+        y-offset (- cy (/ screen-height 2))]
+    [(- (- px x-offset) (/ w 2))
+     (- (- py y-offset) (/ h 2))]))
+
+(defprotocol IRenderer
+  (render [this world] "Renders the World"))
+
+(defrecord Renderer [sprite-batch textures]
+  IRenderer
+  (render [_ world]
+    (let [screen-width (.getWidth Gdx/graphics)
+          screen-height (.getHeight Gdx/graphics)
+          center (get-in world [:entities :player :position])
+          [cx cy _] center
+          range-x (map int (range
+                            (math/floor (- cx (/ (/ screen-width 100) 1.5)))
+                            (math/ceil (+ cx (/ (/ screen-width 100) 1.5)))))
+          range-y (map int (range
+                            (math/floor (- cy (/ (/ screen-height 80) 1.5)))
+                            (math/ceil (+ cy (/ (/ screen-height 80) 1.5)))))]
+
+      ;; Clear Screen
+      (doto (Gdx/gl)
+        (.glClear GL10/GL_COLOR_BUFFER_BIT))
+
+      ;; Draw
+      (.begin sprite-batch)
+      
+      ;; Draw Visable Terrain 
+      (doseq [y (reverse range-y)
+              x range-x]
+        (let [terrain-type (get-in world [:terrain [x y]])
+              [x y] (draw-position screen-width screen-height center [x y 0] 100 120)]
+          (when terrain-type
+            (.draw sprite-batch (terrain-type textures) (float x) (float y)))))
+
+      ;; Draw Player
+      (let [[x y] (draw-position screen-width screen-height center [cx cy 0] 77 91)]
+        (.draw sprite-batch
+               (:character-horn-girl textures) (float x) (float y)))
+      (.end sprite-batch)
+      )))
+
+(defn create-renderer [] (Renderer. (SpriteBatch.) (planetcute-textures)))
